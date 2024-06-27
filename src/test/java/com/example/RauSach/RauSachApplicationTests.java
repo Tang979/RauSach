5617d47cc40a9dcb8fce489a1ec066dc5602278c
Tang979 <57886978+Tang979@users.noreply.github.com>
2024-06-27 11:01:52 +0700

    newProject

---
+++ src/test/java/com/example/RauSach/RauSachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RauSach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RauSachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
