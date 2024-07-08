cc81b20f76b2c85bd6bd873c71227b2b4048a9fa
DoanVanMinh-2180606677 <139115619+DoanVanMinh-2180606677@users.noreply.github.com>
2024-07-08 22:26:08 +0700

    login,forgot,qc

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -20,3 +20,41 @@
             modal.style.display = "none";
         }
     }
+/*
+-----------------------------------------------------*/
+/*
+// Get the modal
+var modal = document.getElementById("adModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Function to show the modal
+function showModal() {
+    modal.style.display = "block";
+}
+
+// Function to hide the modal and set the flag in localStorage
+function hideModal() {
+    modal.style.display = "none";
+    localStorage.setItem('adShown', 'true');
+}
+
+// When the page loads, check if the ad has been shown before
+window.onload = function() {
+    if (!localStorage.getItem('adShown')) {
+        showModal();
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    hideModal();
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        hideModal();
+    }
+}*/
