211ef762eb314415e4b341197482b653d9018518
Tang979 <57886978+Tang979@users.noreply.github.com>
2024-07-09 12:15:10 +0700

    Tim san pham theo category

--- src/main/java/com/example/RauSach/repository/ProductRepository.java
+++ src/main/java/com/example/RauSach/repository/ProductRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.RauSach.model.Category;
 import com.example.RauSach.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,String>{
+    List<Product> findByCategory(Category category);
     List<Product> findByNameContainingIgnoreCase(@Param("keyword") String keyword);
 }
