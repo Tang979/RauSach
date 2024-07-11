edbd499bb1fa3164bb8fd7cc9f08390721bee6a4
Tang979 <57886978+Tang979@users.noreply.github.com>
2024-07-11 12:44:35 +0700

    final

--- src/main/java/com/example/RauSach/controller/CartController.java
+++ src/main/java/com/example/RauSach/controller/CartController.java
@@ -1,6 +1,4 @@
-/*
 package com.example.RauSach.controller;
-
 
 import com.example.RauSach.model.CartItem;
 import com.example.RauSach.model.CartProduct;
@@ -42,42 +40,4 @@ public class CartController {
         cartService.clearCart();
         return "redirect:/cart";
     }
-
-    @GetMapping("/payment/{id}")
-    public String home(Model model,@PathVariable Long id){
-        Order orderCustom = OrderService.getbyid(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
-        List<CartItem> cartItems = cartService.getCartItems();
-        List<CartItem> cartProducts = cartService.getCartItems();
-        model.addAttribute("cartItems", cartItems);
-        model.addAttribute("cartProduct", cartProducts);
-        model.addAttribute("orders",orderCustom);
-// Calculate the total price
-
-        long totalPriceProduct = cartProducts.stream()
-                .filter(cartItem -> cartItem.getProduct() != null)
-                .mapToLong(cartItem -> cartItem.getProduct().getPrice().longValue() * cartItem.getQuantity())
-                .sum();
-        model.addAttribute("totalPriceProduct", totalPriceProduct);
-        // Calculate the total price for pets
-        long totalPricePet = cartItems.stream()
-                .filter(cartItem -> cartItem.getProduct() != null)
-                .mapToLong(cartItem -> cartItem.getProduct().getPrice().longValue() * cartItem.getQuantity())
-                .sum();
-        model.addAttribute("totalPricePet", totalPricePet);
-        long total = totalPriceProduct + totalPricePet;
-        model.addAttribute("total", total);
-        return "Payment/createOrder";
-    }
-
-    //payment
-    // Chuyển hướng người dùng đến cổng thanh toán VNPAY
-    @PostMapping("/payment/{id}")
-    public String submidOrder(
-            HttpServletRequest request, @PathVariable Long id, @Valid @ModelAttribute("orders") Order_Custom orderCustom){
-        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
-        String vnpayUrl = vnPayService.createOrder(request,orderCustom.getTotalAmount(),orderCustom.getId().toString(), baseUrl);
-        return "redirect:" + vnpayUrl;
-    }
 }
-*/
